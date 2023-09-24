f55051cc999206d873f1702cb75d26f79fc170f6
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-24 11:12:05 +0200

    Create user.schema.ts

---
+++ api/src/modules/user/schema/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  creationDate: Date;
+
+  @Prop({ default: false })
+  loginByGoogle: boolean;
+
+  @Prop({ default: false })
+  emailVerified: boolean;
+
+  @Prop()
+  emailVerificationToken?: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
