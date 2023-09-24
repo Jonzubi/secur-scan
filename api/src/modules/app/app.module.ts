aebd8515fbbab99b8a954dd70f85f07757c1a505
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-24 11:16:17 +0200

    add: UserModule

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config/dist';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/env/${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_URL'),
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
