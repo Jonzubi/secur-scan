5b0c28b995f20dd73963585a9827f821c5b695b5
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-25 17:33:09 +0200

    fix: Ahora saco todo del paquete npm

--- api/src/modules/user/user.service.ts
+++ api/src/modules/user/user.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schema/user.schema';
 import { Model } from 'mongoose';
-import { IUser} from '@shared/interfaces/user/user'
+import { IUser } from '@jonzubi/securscan-shared/interfaces/user/user';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async createUser(user: IUser) {
     const newUser = new this.userModel(user);
