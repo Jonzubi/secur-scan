b5d935bb27a9eb85588ade3d25d77af3b3f3c62f
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-26 20:49:38 +0200

    add: AuthModule

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config/dist';
 import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UserModule } from '../user/user.module';
       }),
     }),
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
