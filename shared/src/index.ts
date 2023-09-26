5b658af95f0cd1895d9ab2a9dca18a3d63a80807
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-26 20:39:35 +0200

    add: Shared version 0.0.12

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,2 @@
 export { Tier } from './interfaces/user/tier';
 export { ICreateUser, IUser } from './interfaces/user/user';
-export { hashPassword, comparePassword, randomPassword } from './functions/bcrypt';
