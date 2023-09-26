6b85f2fef4cd345064883de45a79f403f4ddd6d6
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-26 20:57:34 +0200

    add: MailModule

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config/dist';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { AuthModule } from '../auth/auth.module';
     }),
     UserModule,
     AuthModule,
+    MailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
