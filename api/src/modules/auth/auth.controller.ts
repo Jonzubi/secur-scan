e1d616ebcc0ae703288d98a357b4a78b726b5179
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-29 13:43:41 +0200

    add: /profile endpoint

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { IUser } from '@jonzubi/securscan-shared';
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { ISignIn, IUser } from '@jonzubi/securscan-shared';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { Public } from 'src/decorators/IsPublic';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 import { HttpService } from 'src/services/http-service/http-service.service';
 import { Response } from 'express';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,9 +27,23 @@ export class AuthController {
 
   @Post('login')
   @Public()
-  async signIn(@Body() user: IUser, @Res() res: Response): Promise<any> {
+  async signIn(@Body() user: ISignIn, @Res() res: Response): Promise<any> {
     const userData = await this.authService.signIn(user);
     res.status(HttpStatus.OK);
     res.send(userData);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  async getProfile(@Req() req, @Res() res: Response) {
+    const { email } = req.user;
+    const foundUser = await this.userService.findUserByEmail(email);
+    if (foundUser === null) throw new UnauthorizedException();
+    const { username } = foundUser;
+    res.status(HttpStatus.OK);
+    res.send({
+      email,
+      username,
+    });
   }
 }
