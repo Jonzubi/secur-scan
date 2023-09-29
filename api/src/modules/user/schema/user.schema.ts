60af5abd154893dad2059c61866f1ce03f2e5375
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-29 13:36:08 +0200

    fix: Quito /dist para importar mi modulo

--- api/src/modules/user/schema/user.schema.ts
+++ api/src/modules/user/schema/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { Tier } from '@jonzubi/securscan-shared/dist';
+import { Tier } from '@jonzubi/securscan-shared';
 
 export type UserDocument = HydratedDocument<User>;
 
--- api/src/modules/user/user.service.ts
+++ api/src/modules/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schema/user.schema';
 import { Model } from 'mongoose';
-import { ICreateUser, IUser, Tier } from '@jonzubi/securscan-shared/dist';
+import { ICreateUser, IUser, Tier } from '@jonzubi/securscan-shared';
 import { hashPassword } from 'src/utils/functions/bcrypt';
 
 @Injectable()
