1e784b8b8fcf8d402a409725d027b9b67a93a770
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-29 13:04:10 +0200

    add: httpService to be able to fetch data

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { HttpService } from 'src/services/http-service/http-service.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    HttpService,
+  ],
 })
 export class AuthModule {}
