b9cfff0a6d70bc308971e93e69afe6a4af38d742
Jon Zubizarreta <jonzubi97@gmail.com>
2023-09-29 13:39:15 +0200

    add: /login endpoint

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { IUser } from '@jonzubi/securscan-shared';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Public } from 'src/decorators/IsPublic';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { HttpService } from 'src/services/http-service/http-service.service';
+import { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private httpService: HttpService,
+  ) {}
+
+  @Post('login')
+  @Public()
+  async signIn(@Body() user: IUser, @Res() res: Response): Promise<any> {
+    const userData = await this.authService.signIn(user);
+    res.status(HttpStatus.OK);
+    res.send(userData);
+  }
+}
--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { comparePassword } from 'src/utils/functions/bcrypt';
+import { ISignIn } from '@jonzubi/securscan-shared';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(user: ISignIn) {
+    const { password, email } = user;
+    const foundUser = await this.userService.findUserByEmail(email);
+
+    if (foundUser === null) throw new UnauthorizedException();
+    if (foundUser.loginByGoogle) throw new UnauthorizedException();
+
+    const isMatch = comparePassword(password, foundUser.password);
+    if (!isMatch) throw new UnauthorizedException();
+    if (!foundUser.emailVerified)
+      throw new UnauthorizedException('errors.emailNotVerified');
+
+    const { access_token } = await this.jwtSign({
+      email,
+      _id: foundUser._id.toString(),
+    });
+    return {
+      access_token,
+      email,
+      username: foundUser.username,
+    };
+  }
+
+  async jwtSign(user: { email: string; _id: string }) {
+    const { email, _id } = user;
+    const payload = { email, sub: _id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
