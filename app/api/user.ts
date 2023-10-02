28ad3f19322f5fb133b143a0aa0dfca8f3c7117c
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-02 16:20:36 +0200

    create: user API calls

---
+++ app/api/user.ts
@@ -0,0 +1,7 @@
+import { getHeaderWithAccessToken } from 'utils/functions';
+
+export const getProfile = (token: string) =>
+  fetch(
+    `${process.env.EXPO_PUBLIC_API_URL}/auth/profile`,
+    getHeaderWithAccessToken(token),
+  );
