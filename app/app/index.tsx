4edbc829fb603d338a8af1da35d94bc14ecf943b
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-02 15:33:03 +0200

    add: Linter in app

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const index = () => {
   return (
     <View>
       <Text>index</Text>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
