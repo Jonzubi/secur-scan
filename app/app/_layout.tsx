741ceded968258e06aabe09944635a87badacfbf
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-02 16:07:44 +0200

    feat: Splashscreen haciendo tiempo
    Esto lo voy a utilizar as tarde para autentificar el usuario

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Stack, SplashScreen } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 const _layout = () => {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setReady(true);
+      SplashScreen.hideAsync();
+    }, 5000);
+  }, []);
+
+  if (!ready) {
+    return null;
+  }
+
   return <Stack />;
 };
 
