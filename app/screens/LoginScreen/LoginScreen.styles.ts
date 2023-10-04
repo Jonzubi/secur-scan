a2881ead5333ee118193c6e383f541b639c5e8d7
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-04 21:13:59 +0200

    Create LoginScreen.styles.ts

---
+++ app/screens/LoginScreen/LoginScreen.styles.ts
@@ -0,0 +1,12 @@
+import Colors from '../../constants/Colors';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BACKGROUND,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+});
