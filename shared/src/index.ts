15f2f38749f9251603c8ce2ea9861aa18bab3416
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-07 14:24:54 +0200

    shared version 0.0.16

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,4 @@
 export { Tier } from './interfaces/user/tier';
 export { ICreateUser, IUser } from './interfaces/user/user';
 export { ISignIn } from './interfaces/user/auth';
+export { isEmail } from './functions/email'
