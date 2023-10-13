e8939549d52c760b655bdf44b8b8c95ddeb4d8f3
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-13 16:21:05 +0200

    feat: RegisterScreen maquetado

--- app/screens/RegisterScreen/RegisterScreen.styles.ts
+++ app/screens/RegisterScreen/RegisterScreen.styles.ts
@@ -1,3 +1,38 @@
+import Colors from '../../constants/colors';
 import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+import colors from '../../constants/colors';
 
-export default StyleSheet.create({});
+const { width } = Dimensions.get('window');
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BACKGROUND,
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+  },
+  logo: {
+    width: 500 * width * 0.001,
+    height: 500 * width * 0.001,
+    alignSelf: 'center',
+  },
+  form: {
+    marginTop: 30,
+    gap: 15,
+  },
+  registerView: {
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'center',
+  },
+  registerText: {
+    color: colors.MAIN_RED,
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  youNewText: {
+    color: colors.GRAY,
+    fontWeight: '600',
+  },
+});
--- app/screens/RegisterScreen/RegisterScreen.tsx
+++ app/screens/RegisterScreen/RegisterScreen.tsx
@@ -1,10 +1,80 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View } from 'react-native';
+import { useRef, useState } from 'react';
+import styles from './RegisterScreen.styles';
+import Logo from '../../components/Logo/Logo';
+import { Input } from '@rneui/themed';
+import Colors from '../../constants/colors';
+import { useTranslation } from 'react-i18next';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import CustomToast, {
+  ToastType,
+} from '../../components/CustomToast/CustomToast';
+import { useModal } from '../../hooks/useModal';
 
 const RegisterScreen = () => {
+  const { t } = useTranslation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { modalText, setModalText, setShowModal, showModal } = useModal(
+    t('auth.incorrectLogin'),
+  );
+  const emailRef = useRef<any>(null);
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
+    <View style={styles.container}>
+      <Logo imageStyle={styles.logo} />
+      <View style={styles.form}>
+        <Input
+          inputStyle={{ color: Colors.WHITE }}
+          placeholder={t('registerScreen.emailPlaceholder')}
+          placeholderTextColor={Colors.GRAY}
+          leftIcon={
+            <AntDesign name="mail" size={24} color={Colors.SECONDARY_RED} />
+          }
+          onChangeText={setEmail}
+          errorMessage={errorEmail}
+          ref={emailRef}
+        />
+        <Input
+          inputStyle={{ color: Colors.WHITE }}
+          placeholder={t('registerScreen.passwordPlaceholder')}
+          placeholderTextColor={Colors.GRAY}
+          secureTextEntry={true}
+          leftIcon={
+            <AntDesign name="lock" size={24} color={Colors.SECONDARY_RED} />
+          }
+          onChangeText={setPassword}
+          errorMessage={errorPassword}
+        />
+        <Input
+          inputStyle={{ color: Colors.WHITE }}
+          placeholderTextColor={Colors.GRAY}
+          placeholder={t('registerScreen.repeatPasswordPlaceholder')}
+          secureTextEntry={true}
+          leftIcon={
+            <AntDesign name="lock" size={24} color={Colors.SECONDARY_RED} />
+          }
+          onChangeText={setConfirmPassword}
+          errorMessage={errorPassword}
+        />
+        <SubmitButton
+          title={t('registerScreen.submitButton')}
+          isLoading={isLoading}
+          handlePress={() => {}}
+        />
+        <CustomToast
+          type={ToastType.ERROR}
+          closeModal={() => setShowModal(false)}
+          visible={showModal}
+          text={modalText}
+        />
+      </View>
     </View>
   );
 };
