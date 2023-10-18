48816f445ce7282bfbe261493ff3abf0550e9765
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-18 22:24:10 +0200

    create: HomeScreen

---
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import {} from 'react';
+import styles from './HomeScreen.styles';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
