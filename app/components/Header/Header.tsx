de3341da896fcdca065330030fcf860ca4d961a8
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-18 22:39:36 +0200

    fix: Condiciono que cabecera renderizar segun tamaño

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,18 +1,31 @@
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import React from 'react';
 import styles from './Header.styles';
 import HeaderLogo from '../../components/HeaderLogo/HeaderLogo';
+import Logo from '../../components/Logo/Logo';
+
+const { width } = Dimensions.get('window');
 
 const Header = () => {
   return (
     <View style={styles.container}>
-      <HeaderLogo
-        imageStyle={{
-          height: 400,
-          width: 400,
-          marginLeft: -55,
-        }}
-      />
+      {width > 1400 ? (
+        <HeaderLogo
+          imageStyle={{
+            height: 400,
+            width: 400,
+            marginLeft: -55,
+          }}
+        />
+      ) : (
+        <Logo
+          imageStyle={{
+            height: 50,
+            width: 50,
+            marginLeft: -20,
+          }}
+        />
+      )}
     </View>
   );
 };
