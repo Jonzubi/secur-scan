78fae4364c9c0b1f6bdfc72f0b7e598320bd6774
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-26 20:40:42 +0200

    add: IGetProfile

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,4 @@
 export { Tier } from './interfaces/user/tier';
 export { ICreateUser, IUser } from './interfaces/user/user';
-export { ISignIn } from './interfaces/user/auth';
+export { ISignIn, IGetProfile } from './interfaces/user/auth';
 export { isEmail } from './functions/email'
--- shared/src/interfaces/user/auth.ts
+++ shared/src/interfaces/user/auth.ts
@@ -1,4 +1,12 @@
+import { Tier } from "./tier";
+
 export interface ISignIn {
   email: string;
   password: string;
+}
+
+export interface IGetProfile {
+  email: string;
+  tier: Tier;
+  tokens: number;
 }
