2fce3b711554576d10944b174de4e6bd70082135
Jon Zubizarreta <jonzubi97@gmail.com>
2023-10-26 23:03:42 +0200

    add: Token info in ProfileScreen

--- app/screens/ProfileScreen/ProfileScreen.styles.ts
+++ app/screens/ProfileScreen/ProfileScreen.styles.ts
@@ -7,4 +7,19 @@ export default StyleSheet.create({
     backgroundColor: colors.BACKGROUND,
     justifyContent: 'space-between',
   },
+  optionsContainer: {
+    padding: 15,
+  },
+  optionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: colors.GRAY,
+    borderRadius: 10,
+  },
+  optionContainerText: {
+    color: colors.WHITE,
+    fontSize: 15,
+  },
 });
--- app/screens/ProfileScreen/ProfileScreen.tsx
+++ app/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import {} from 'react';
 import styles from './ProfileScreen.styles';
 import LogOutButton from '../../components/LogOutButton/LogOutButton';
+import { useTranslation } from 'react-i18next';
+import { useUserStore } from '../../store/userStore';
 
 const ProfileScreen = () => {
+  const { t } = useTranslation();
+  const { tokens } = useUserStore();
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <View style={styles.optionsContainer}>
+        <TouchableOpacity style={styles.optionContainer}>
+          <Text style={styles.optionContainerText}>
+            {t('profileScreen.myTokens')}
+          </Text>
+          <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
+            <Text style={styles.optionContainerText}>{tokens}</Text>
+            <Image
+              source={require('../../assets/icons/currency.png')}
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
       <LogOutButton />
     </View>
   );
