a54d4e26c33e82fbcf3282aeafb17563b6cb82e7
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-08 20:46:38 +0100

    add: isIP function to shared

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -3,3 +3,4 @@ export { ICreateUser, IUser } from './interfaces/user/user';
 export { ISignIn, IGetProfile } from './interfaces/user/auth';
 export { ICreateRequest, RequestType } from './interfaces/request/request';
 export { isEmail } from './functions/email'
+export { isIP } from './functions/ip'
