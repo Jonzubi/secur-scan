24b058e149f5c29fc875cc6efb1e9fda47ed7f2f
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-16 22:24:57 +0100

    cut: HeaderLogo

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,33 +1,20 @@
-import { View, Dimensions, Text, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
 import styles from './Header.styles';
-import HeaderLogo from '../../components/HeaderLogo/HeaderLogo';
 import Logo from '../../components/Logo/Logo';
 import { useUserStore } from '../../store/userStore';
-
-const { width } = Dimensions.get('window');
 
 const Header = () => {
   const { tokens } = useUserStore();
   return (
     <View style={styles.container}>
-      {width > 500 ? (
-        <HeaderLogo
-          imageStyle={{
-            height: 400,
-            width: 400,
-            marginLeft: -55,
-          }}
-        />
-      ) : (
-        <Logo
-          imageStyle={{
-            height: 50,
-            width: 50,
-            marginLeft: -20,
-          }}
-        />
-      )}
+      <Logo
+        imageStyle={{
+          height: 50,
+          width: 50,
+          marginLeft: -20,
+        }}
+      />
       <View style={styles.tokenContainer}>
         <Text style={styles.tokenText}>{tokens}</Text>
         <Image
