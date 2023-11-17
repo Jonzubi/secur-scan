fd505ba903aca2f8a038ff693a0ce75f0e964b92
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-17 16:45:50 +0100

    add: ResolveDnsScreen

---
+++ app/screens/ResolveDnsScreen/ResolveDnsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import {} from 'react';
+
+const ResolveDnsScreen = () => {
+  return (
+    <View>
+      <Text>ResolveDnsScreen</Text>
+    </View>
+  );
+};
+
+export default ResolveDnsScreen;
