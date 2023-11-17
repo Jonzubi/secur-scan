e6d7b12a525f389ec42908e678e4648f631e1328
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-17 17:26:00 +0100

    add: Maquetacion ResolveDNSScreen

--- app/screens/ResolveDnsScreen/ResolveDnsScreen.tsx
+++ app/screens/ResolveDnsScreen/ResolveDnsScreen.tsx
@@ -1,10 +1,41 @@
 import { View, Text } from 'react-native';
-import {} from 'react';
+import styles from './ResolveDnsScreen.styles';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Input } from '@rneui/themed';
+import colors from '../../constants/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
 
 const ResolveDnsScreen = () => {
+  const { t } = useTranslation();
+  const [domain, setDomain] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = () => {};
   return (
-    <View>
-      <Text>ResolveDnsScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.formTitle}>{t('resolveDnsScreen.title')}</Text>
+        <Input
+          inputStyle={{ color: colors.WHITE }}
+          placeholder={t('resolveDnsScreen.domainPlaceholder')}
+          placeholderTextColor={colors.GRAY}
+          leftIcon={
+            <MaterialCommunityIcons
+              name="web"
+              size={24}
+              color={colors.MAIN_RED}
+            />
+          }
+          onChangeText={setDomain}
+        />
+        <SubmitButton
+          isLoading={isLoading}
+          handlePress={handleSubmit}
+          title={t('resolveDnsScreen.submitButton')}
+        />
+      </View>
     </View>
   );
 };
