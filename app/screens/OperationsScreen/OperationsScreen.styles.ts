7db045cd30f57dd1640befa101f249591bd00c1b
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-23 23:08:13 +0100

    add: List in operationScreen

---
+++ app/screens/OperationsScreen/OperationsScreen.styles.ts
@@ -0,0 +1,9 @@
+import colors from '../../constants/colors';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.BACKGROUND,
+  },
+});
--- app/screens/OperationsScreen/OperationsScreen.tsx
+++ app/screens/OperationsScreen/OperationsScreen.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
+import { useRequests } from '../../hooks/useRequests';
+import styles from './OperationsScreen.styles';
+import { FlashList } from '@shopify/flash-list';
+import Operation from '../../components/Operation/Operation';
 
 const OperationsScreen = () => {
+  const { data, error, loading } = useRequests();
+
   return (
-    <View>
-      <Text>OperationsScreen</Text>
+    <View style={styles.container}>
+      <FlashList
+        data={data}
+        renderItem={({ item }) => <Operation {...item} />}
+        keyExtractor={(item) => item._id}
+        estimatedItemSize={100}
+      />
     </View>
   );
 };
