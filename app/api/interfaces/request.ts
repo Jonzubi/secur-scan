23de973b609f8877462856cd1ce5962b9f9f1719
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-23 23:20:35 +0100

    fix: add status to IGetRequest

--- app/api/interfaces/request.ts
+++ app/api/interfaces/request.ts
@@ -1,10 +1,11 @@
-import { RequestType } from '@jonzubi/securscan-shared';
+import { RequestStatus, RequestType } from '@jonzubi/securscan-shared';
 
 export type IGetRequest = {
   _id: string;
   userId: string;
   requestType: RequestType;
   ipToScan: string;
+  status: RequestStatus;
   __v: number;
   requestResolve: IResolve[];
 };
