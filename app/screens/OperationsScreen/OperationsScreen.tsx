9e24c5a922fa0714b0fc4b896fd8a70c3ec8a878
Jon Zubizarreta <jonzubi97@gmail.com>
2023-11-23 23:30:09 +0100

    feat: OperationScreen can refresh by swipe up

--- app/screens/OperationsScreen/OperationsScreen.tsx
+++ app/screens/OperationsScreen/OperationsScreen.tsx
@@ -1,21 +1,33 @@
-import { View } from 'react-native';
-import React from 'react';
+import { View, RefreshControl, ScrollView } from 'react-native';
+import React, { useState } from 'react';
 import { useRequests } from '../../hooks/useRequests';
 import styles from './OperationsScreen.styles';
 import { FlashList } from '@shopify/flash-list';
 import Operation from '../../components/Operation/Operation';
 
 const OperationsScreen = () => {
-  const { data, error, loading } = useRequests();
+  const { data, error, loading, refetch } = useRequests();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    refetch().finally(() => setRefreshing(false));
+  }, [refetch]);
 
   return (
     <View style={styles.container}>
-      <FlashList
-        data={data}
-        renderItem={({ item }) => <Operation {...item} />}
-        keyExtractor={(item) => item._id}
-        estimatedItemSize={100}
-      />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <FlashList
+          data={data}
+          renderItem={({ item }) => <Operation {...item} />}
+          keyExtractor={(item) => item._id}
+          estimatedItemSize={100}
+        />
+      </ScrollView>
     </View>
   );
 };
