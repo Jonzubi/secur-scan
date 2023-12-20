4234714e2b637c701a6003c3a4b7014e61335ab1
Jon Zubizarreta <jonzubi97@gmail.com>
2023-12-20 16:45:55 +0100

    feat(APP): If status is not defined, render a question mark

--- app/components/StatusToIcon/StatusToIcon.tsx
+++ app/components/StatusToIcon/StatusToIcon.tsx
@@ -2,6 +2,7 @@ import { View, ActivityIndicator } from 'react-native';
 import React from 'react';
 import { RequestStatus } from '@jonzubi/securscan-shared';
 import colors from '../../constants/colors';
+import { AntDesign } from '@expo/vector-icons';
 
 interface IStatusToIcon {
   status: RequestStatus;
@@ -10,6 +11,9 @@ const StatusToIcon = ({ status }: IStatusToIcon) => {
   if (status === RequestStatus.PENDING) {
     return <ActivityIndicator color={colors.SECONDARY_RED} />;
   }
+
+  if (!status)
+    return <AntDesign name="question" size={24} color={colors.WHITE} />;
 
   const RequestStatusToColor = {
     [RequestStatus.SUCCESS]: colors.MAIN_GREEN,
